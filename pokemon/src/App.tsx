2085bc1413b4dd4ba052e779aa74537c09c2ffef
danaasf <danaasfour35@gmail.com>
2022-12-04 19:25:13 +0200

    types and pokelist

--- pokemon/src/App.tsx
+++ pokemon/src/App.tsx
@@ -4,8 +4,6 @@ import './App.css';
 import { Pokemon } from './types';
 import { PokemonsList } from './Components/Pages/Main/PokemonsList';
 
-
-
 function App() {
 
   const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
--- pokemon/src/Components/Pages/Main/PokemonsList.tsx
+++ pokemon/src/Components/Pages/Main/PokemonsList.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import '../../../App.css';
 import { Pokemon } from '../../../types';
 import { PokemonIcon } from './PokemonIcon';
+import './styles.css'
 
 export interface PokemonsListProp {
     pokemons: Pokemon[];
     setPokemons: React.Dispatch<React.SetStateAction<Pokemon[]>>;
-
 }
 
 export const PokemonsList: React.FC<PokemonsListProp> = ({
@@ -15,58 +15,35 @@ export const PokemonsList: React.FC<PokemonsListProp> = ({
 }) => {
    
     const [currentInput, setCurrentInput] = React.useState<string>('');
+    const getInitialIds = () => {
+        const idSet = new Set();
+        while(idSet.size < 3) {
+            idSet.add(Math.floor(Math.random() * 151));
+        }
+        return idSet;
+    }
 
+    const fetchPokemonJSON = async (pokemonId: any): Promise<any> => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then((res) => res.json());
     
-    const addRandomCharacter = async() => {
-       const randomNumber1 = Math.floor(Math.random() * 151);
-       const randomNumber2 = Math.floor(Math.random() * 151);
-       const randomNumber3 = Math.floor(Math.random() * 151);
-       let p1,p2,p3;
-       try {
-            p1 = await fetch(`https://pokeapi.co/api/v2/pokemon//${randomNumber1}`);
-            p1 = await p1.json();
-        
-            
-            p2 = await fetch(`https://pokeapi.co/api/v2/pokemon//${randomNumber2}`);
-            p2 = await p2.json();
+    const initPokemonCollection = async() => {
+        const ids = getInitialIds();
+        const promises: Promise<any> [] = [];
 
-            p3 = await fetch(`https://pokeapi.co/api/v2/pokemon//${randomNumber3}`);
-            p3 = await p3.json();
-       } catch(e) {
-            console.error(e);
-       }
+        ids.forEach((id) => promises.push(fetchPokemonJSON(id)));
 
-       if (p1 && p2 && p3 && p1?.name !== '' && p2?.name !== '' && p3?.name !== '') {
-
-        const newPokemon1: Pokemon = {
-            name: p1.name,
-            image: p1.sprites.front_default,
-        }
-        const newPokemon2: Pokemon = {
-            name: p2.name,
-            image: p2.sprites.front_default,
-        }
-        const newPokemon3: Pokemon = {
-            name: p3.name,
-            image: p3.sprites.front_default,
-        }
-        const pokemonsList = [newPokemon1, newPokemon2, newPokemon3];
-        setPokemons(pokemonsList);
-
-    }
+        const pokemons = await Promise.all(promises).then((res): Pokemon[] => res.map((pokemon: any) => {name: pokemon.name; image: pokemon.sprites.front_default}));
+        setPokemons(pokemons);
     }
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        // What is the problem with this approach? Read about debouncing.
-        e.preventDefault();
-        setCurrentInput(e.target.value); // Hint <- this is the problem. think about state and re-rendering.
-    }
+  
+    useEffect(() => {
+        initPokemonCollection();
+    }, []);
 
     return(
         <> 
             <h2> Pokemons Page </h2>
             <div className='page-container'>
-                <input onChange={handleInputChange} />
                 </div> 
           
             <div className='pokemon-list'>
@@ -75,6 +52,7 @@ export const PokemonsList: React.FC<PokemonsListProp> = ({
                 return (
                 <div className='row pokemon' key={pokemon.name}>
                     <span>{pokemon.name}</span>
+                    <span>{pokemon.image}</span>
                 </div>
                 )
             })
