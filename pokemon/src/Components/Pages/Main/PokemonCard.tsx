408a99b066bc1023a0848abbccd05a5d54c554f4
danaasf <danaasfour35@gmail.com>
2022-12-08 00:11:26 +0200

    updated homepage with scrollbar and statistics

--- pokemon/src/Components/Pages/Main/PokemonCard.tsx
+++ pokemon/src/Components/Pages/Main/PokemonCard.tsx
@@ -27,15 +27,19 @@ export const PokemonCard: React.FC<PokemonIconProps> = ({fetchPokemonJSON, pokem
         const type=pokemonJSON.types.map((e:any)=>{
             return e.type.name;
         })
-        let stat:number[] = []
+        
+        const hp= pokemonJSON.stats[0].base_stat;
+        const attack= pokemonJSON.stats[1].base_stat;
+        const defense= pokemonJSON.stats[2].base_stat;
+        const special_attack= pokemonJSON.stats[3].base_stat;
+        const special_defense= pokemonJSON.stats[4].base_stat;
+        const speed= pokemonJSON.stats[5].base_stat;
+        let stat= new Map<string,number>();
 
-        // pokemonJSON.stats.ForEach((e:any)=>{
-        //     //const temp = someObj[field as keyof typeof someObj]
-
-        //     //stats[(e.stat.name) as keyof tyeof stats]=e.base_stat;
-        // })
-        setPokemonCard({name,image,weight,height,type,stat});
+        setPokemonCard({name,image,weight,height,type,hp,attack,defense,special_attack,special_defense,speed});
     },[pokemonJSON])
+
+
 
     return(
         <div className='right-content'>
@@ -44,10 +48,20 @@ export const PokemonCard: React.FC<PokemonIconProps> = ({fetchPokemonJSON, pokem
             <img src={pokemonCard.image} />
             <p><h2>height:</h2> {pokemonCard?.height}</p>
             <p><h2>weight:</h2> {pokemonCard?.weight}</p>
-            <p><h2>type:</h2> {pokemonCard?.type.join(', ')} </p></div>) : "No Pokemon Selected"}
+            <p><h2>type:</h2> {pokemonCard?.type.join(', ')} </p>
+            <p><h2>hp:</h2> {pokemonCard?.hp}</p>
+            <p><h2>attack:</h2> {pokemonCard?.attack}</p>
+            <p><h2>special attack:</h2> {pokemonCard?.special_attack}</p>
+            <p><h2>defense:</h2> {pokemonCard?.defense}</p>
+            <p><h2>special defense:</h2> {pokemonCard?.special_defense}</p>
+            <p><h2>speed:</h2> {pokemonCard?.speed}</p>
+            </div> 
+            ) : "No Pokemon Selected"}
+            
             <br></br> <br></br> 
-                <div className='choose-poke'>
-            {pokemonCard? (<button type="button" className="btn btn-light">I choose you</button>): " " }
+    
+                <div className='choose-poke'>           
+            {pokemonCard? ( <a href='google.com'> <button type="button" className="btn btn-light">I choose you</button></a>): " " }
                 </div>
         </div>
         
--- pokemon/src/types.ts
+++ pokemon/src/types.ts
@@ -10,5 +10,16 @@ export interface CardInfo {
     type: Array<string>;
     weight: number;
     height: number;
-    stat: Array<number>;
+    hp: number;
+    attack: number;
+    defense:number;
+    special_attack: number;
+    special_defense: number;
+    speed: number;
+}
+
+export interface Battle {
+    stats: Array<number>;
+    result: boolean;
+
 }
